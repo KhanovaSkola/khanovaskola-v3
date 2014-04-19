bd597413d644175eb06e425b5d6a17538d0a94dc
Mikulas <rullaf@gmail.com>
2014-04-19 22:07:37 +0200

    add gists

---
+++ migrations/20140419220151_gists.php
@@ -0,0 +1,20 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+
+/** @see http://docs.phinx.org/en/latest/migrations.html#creating-a-new-migration */
+class Gists extends AbstractMigration
+{
+
+    public function up()
+    {
+	    $this->table('gists')
+		    ->addColumn('name', 'string', ['limit' => 250])
+		    ->addColumn('text', 'text')
+		    ->addColumn('created_at', 'datetime')
+		    ->addIndex(['name'], ['unique' => TRUE, 'name' => 'name'])
+		    ->save();
+    }
+
+}
