6e8bfcabbae6f749e11dd3c7e2995f565f9980fc
Mikulas <rullaf@gmail.com>
2014-04-20 10:58:42 +0200

    add gist renderer read-only mode

--- app/components/GistRenderer/GistRenderer.php
+++ app/components/GistRenderer/GistRenderer.php
@@ -11,21 +11,30 @@ class GistRenderer extends Control
 	/** @var Gist */
 	private $gist;
 
+	/** @var bool */
+	private $editable = FALSE;
+
 	public function setGist(Gist $gist)
 	{
 		$this->gist = $gist;
 	}
 
+	public function setEditable($bool = TRUE)
+	{
+		$this->editable = $bool;
+	}
+
 	protected function renderDefault()
 	{
 		$this->template->gist = $this->gist;
+		$this->template->editable = $this->editable;
 	}
 
 	public function handleSave()
 	{
-		if (!$this->presenter->isAjax())
+		if (!$this->presenter->isAjax() || !$this->editable)
 		{
-			$this->error();
+			$this->presenter->error();
 		}
 		$text = $this->presenter->getRequest()->getPost()['text'];
 		$this->gist->text = $text;
