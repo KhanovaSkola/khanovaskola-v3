bf6abe9dd264f931d8512715df107417d4ed1daf
Mikulas <rullaf@gmail.com>
2014-04-24 17:05:42 +0200

    [dev] move web tools to /tools, rename adminer to db

---
+++ www/tools/beanstalk/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config = [
+	// List of servers available for all users
+	'servers' => ['localhost:13000'],
+	// Saved samples jobs are kept in this file, must be writable
+	'storage' => __DIR__ . '/storage.json',
+	'version' => '1.4.1',
+];
