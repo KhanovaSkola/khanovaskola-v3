bead5920a0aa7b8cfbc75d30678054239b8958e6
Mikulas <rullaf@gmail.com>
2014-04-24 11:33:30 +0200

    disable forced debug mode

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,8 @@ require __DIR__ . '/config/Configurator.php';
 
 $configurator = new App\Configurator;
 
-$configurator->setDebugMode(TRUE || ['192.168.56.1']);
+$configurator->setDebugMode(['127.0.0.1', '192.168.56.1']);
+//$configurator->setDebugMode($configurator::PRODUCTION);
 $configurator->enableDebugger(__DIR__ . '/../log', 'rullaf+errors@gmail.com');
 
 $configurator->createRobotLoader()
