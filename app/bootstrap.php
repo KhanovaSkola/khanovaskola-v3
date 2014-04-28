fbabcebf2344a8a89f7eb94c7c3cabd1b900b3e2
Mikulas <rullaf@gmail.com>
2014-04-28 16:50:32 +0200

    [dev] enable debug mode on travis

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,9 +4,7 @@ $loader = require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/config/Configurator.php';
 
 $configurator = new App\Configurator;
-
 $configurator->setDebugMode(['packer-virtualbox', '127.0.0.1', '192.168.56.1']);
-//$configurator->setDebugMode($configurator::PRODUCTION);
 $configurator->enableDebugger(__DIR__ . '/../log', 'rullaf+errors@gmail.com');
 
 $configurator->createRobotLoader()
