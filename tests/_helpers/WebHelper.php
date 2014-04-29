9fe4927d7a33581adf4c9136cbe9ad89e4d71913
Mikulas <rullaf@gmail.com>
2014-04-29 08:29:29 +0200

    [test] add fb/google login acceptance tests

--- tests/_helpers/WebHelper.php
+++ tests/_helpers/WebHelper.php
@@ -1,8 +1,35 @@
 <?php
 namespace Codeception\Module;
 
-// here you can define custom functions for WebGuy 
+// here you can define custom functions for WebGuy
 
 class WebHelper extends \Codeception\Module
 {
+
+	public function logInToFacebook($user, $pass)
+	{
+		/** @var PhpBrowser $phpBrowser */
+		$phpBrowser = $this->getModule('PhpBrowser');
+
+		$phpBrowser->_sendRequest('https://www.facebook.com/login.php');
+		$phpBrowser->submitForm('#login_form', [
+			'email' => $user,
+			'pass' => $pass,
+		]);
+		$phpBrowser->seeInCurrentUrl('welcome');
+	}
+
+	public function logInToGoogle($user, $pass)
+	{
+		/** @var PhpBrowser $phpBrowser */
+		$phpBrowser = $this->getModule('PhpBrowser');
+
+		$phpBrowser->_sendRequest('https://accounts.google.com/ServiceLogin');
+		$phpBrowser->submitForm('#gaia_loginform', [
+			'Email' => $user,
+			'Passwd' => $pass,
+		]);
+		$phpBrowser->seeInCurrentUrl('/settings/personalinfo');
+	}
+
 }
