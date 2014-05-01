88892605c0d42a81a525216bb591aead466e7118
Mikulas <rullaf@gmail.com>
2014-05-01 15:02:48 +0200

    [test] fix search cept only working on CI

--- tests/acceptance/searchCept.php
+++ tests/acceptance/searchCept.php
@@ -2,5 +2,5 @@
 
 $I = new WebGuy($scenario);
 $I->wantTo('search by stem');
-$I->amOnPage('/homepage/search?query=rovnice');
-$I->see('O rovnicích bez rovnic');
+$I->amOnPage('/homepage/search?query=sčítání%20funkce');
+$I->see('Sčítání funkcí');
