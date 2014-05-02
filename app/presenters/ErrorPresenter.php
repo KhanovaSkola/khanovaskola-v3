c69be0aaa08b0638d0f7f7416ed847e409cb4b37
Mikulas <rullaf@gmail.com>
2014-05-02 15:26:58 +0200

    translate error pages

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -18,12 +18,11 @@ final class ErrorPresenter extends BasePresenter
 	{
 		if ($exception instanceof Nette\Application\BadRequestException)
 		{
-			$code = $exception->getCode();
-			// load template 403.latte or 404.latte or ... 4xx.latte
-			$this->setView(in_array($code, [403, 404, 405, 410, 500]) ? $code : '4xx');
-			// log to access.log
-			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
+			$this->setView('4xx');
 
+			$code = $exception->getCode();
+			$this->template->code = $code;
+			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
 		}
 		else
 		{
