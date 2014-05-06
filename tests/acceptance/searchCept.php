9e7f94bdb284ecf16fa338385bf5439d4a9335fe
Mikulas <rullaf@gmail.com>
2014-05-06 17:14:40 +0200

    [test] update search cept

--- tests/acceptance/searchCept.php
+++ tests/acceptance/searchCept.php
@@ -2,5 +2,5 @@
 
 $I = new WebGuy($scenario);
 $I->wantTo('search by stem');
-$I->amOnPage('/homepage/search?query=sčítání%20funkce');
+$I->amOnPage('/search/results?query=scitani+funkce');
 $I->see('Sčítání funkcí');
