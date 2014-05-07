ecd06408bf3ba6a5db367c8c1ab82e3a3ccdb78b
Mikulas <rullaf@gmail.com>
2014-05-07 21:27:22 +0200

    replace mt_rand with rand in blueprint compiler, add answer form

--- app/presenters/ExercisePresenter.php
+++ app/presenters/ExercisePresenter.php
@@ -4,6 +4,7 @@ namespace App\Presenters;
 
 use App\Rme\Blueprint;
 use App\Rme\BlueprintCompiler;
+use Nette\Application\UI\Form;
 
 
 final class ExercisePresenter extends BasePresenter
@@ -28,11 +29,53 @@ final class ExercisePresenter extends BasePresenter
 		}
 	}
 
-	public function renderDefault()
+	private function getExercise($seed = NULL)
 	{
 		$compiler = new BlueprintCompiler();
-		//$compiler->setSeed(1114283787);
-		$this->template->exercise = $compiler->compile($this->blueprint);
+		if ($seed !== NULL)
+		{
+			$compiler->setSeed($seed);
+		}
+
+		return $compiler->compile($this->blueprint);
+	}
+
+	public function renderDefault($seed = NULL)
+	{
+		$exercise = $this->getExercise($seed);
+		$this['answer-seed']->setDefaultValue($exercise->seed);
+		$this->template->exercise = $exercise;
+	}
+
+	public function createComponentAnswer()
+	{
+		$form = new Form();
+
+		$form->addHidden('seed');
+		$form->addText('answer');
+
+		$form->addSubmit('send');
+		$form->onSuccess[] = $this->onSuccessAnswer;
+
+		return $form;
+	}
+
+	public function onSuccessAnswer(Form $form)
+	{
+		$seed = $form['seed']->value;
+		$answer = $form['answer']->value;
+
+		$exercise = $this->getExercise($seed);
+		if ($exercise->verifyAnswer($answer))
+		{
+			$this->flashSuccess('exercise.correct', ['answer' => $answer]);
+			$this->redirect('this', ['seed' => NULL]);
+		}
+		else
+		{
+			$this->flashError('exercise.wrong', ['answer' => $answer]);
+			$this->redirect('this', ['seed' => $seed]);
+		}
 	}
 
 }
