30f3a2d593d0db66afb8de1ff0670b92df17a9f8
Mikulas <rullaf@gmail.com>
2014-05-09 14:37:31 +0200

    mark neo4j persist on update as not implemented

--- app/model/orm/mapper/Neo4jTrait.php
+++ app/model/orm/mapper/Neo4jTrait.php
@@ -2,7 +2,9 @@
 
 namespace App\Orm\Mapper;
 
+use App\NotImplementedException;
 use App\Orm\Entity;
+use App\Orm\TitledEntity;
 use App\Services\Neo4j;
 use Orm\EventArguments;
 use Orm\Events;
@@ -24,16 +26,22 @@ trait Neo4jTrait
 
 	protected function registerOnPersistCreateNodeEvent(Events $events)
 	{
-		$events->addCallbackListener($events::PERSIST_AFTER, function(EventArguments $args) {
-			/** @var Entity $e */
+		$events->addCallbackListener($events::PERSIST_AFTER_INSERT, function(EventArguments $args) {
+			/** @var TitledEntity $e */
 			$e = $args->entity;
 
 			$node = $this->neo4j->makeNode();
 			/** @var Mapper|Neo4jTrait $this */
 			$node->setProperty('eid', $e->id)->save();
+			$node->setProperty('slug', $e->slug)->save();
 
 			$label = $this->neo4j->makeLabel(ucFirst($this->getShortEntityName()));
 			$node->addLabels([$label]);
+		});
+
+		$events->addCallbackListener($events::PERSIST_AFTER_UPDATE, function(EventArguments $args) {
+			// TODO find node and update slug if changed
+			throw new NotImplementedException;
 		});
 	}
 
