99d9355089c480a9f08b6cd4783ac43f51536fab
Mikulas <rullaf@gmail.com>
2014-05-10 16:32:08 +0200

    add funny error images

--- app/presenters/TestPresenter.php
+++ app/presenters/TestPresenter.php
@@ -2,9 +2,16 @@
 
 namespace App\Presenters;
 
+use Nette\Http\IResponse;
+
 
 final class TestPresenter extends BasePresenter
 {
+
+	public function renderE404()
+	{
+		$this->error(NULL, IResponse::S404_NOT_FOUND);
+	}
 
 	public function actionDefault()
 	{
