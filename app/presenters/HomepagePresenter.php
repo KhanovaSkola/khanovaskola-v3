6564fdecd95e065f372b21126cc917611738b8d7
Mikulas <rullaf@gmail.com>
2014-05-15 15:48:59 +0200

    [cs] move example path creating to gists

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -12,19 +12,6 @@ final class HomepagePresenter extends BasePresenter
 
 	public function actionDefault()
 	{
-//		$p = new Path();
-//		$p->list = [
-//			$this->orm->videos->getById(1),
-//			$this->orm->videos->getById(3),
-//			$this->orm->videos->getById(2),
-//			$this->orm->videos->getById(5),
-//		];
-//
-//		$this->orm->paths->attach($p);
-//		$p->getRepository()->getMapper(); // HACK, move events to repository
-//		$this->orm->flush();
-//		dump('done');
-
 		$this->template->video = $this->orm->videos->getById(3);
 
 		$gist = $this->orm->gists->getByName('test');
--- doc/snippets.md
+++ doc/snippets.md
@@ -10,3 +10,20 @@ Create new child of `Task` and enqueue it as:
 $task = new SendMailTask('test', 'mikulas@khanovaskola.cz', ['foo' => 'bar']);
 $this->context->getService('queue')->enqueue($task);
 ```
+
+Create learn path
+-----------------
+
+```php
+$p = new Path();
+$p->list = [
+	$this->orm->videos->getById(1),
+	$this->orm->videos->getById(3),
+	$this->orm->videos->getById(2),
+	$this->orm->videos->getById(5),
+];
+//
+$this->orm->paths->attach($p);
+$p->getRepository()->getMapper(); // HACK, move events to repository
+$this->orm->flush();
+```
