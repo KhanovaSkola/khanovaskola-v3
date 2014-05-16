ec552abdc92fe6b38900bbdd50f9a5b3ec310f08
Mikulas <rullaf@gmail.com>
2014-05-16 14:35:53 +0200

    remove test code in homepage

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -10,29 +10,6 @@ final class HomepagePresenter extends BasePresenter
 
 	public function actionDefault()
 	{
-//		$p = new Path();
-//		$p->list = [
-//			$this->orm->videos->getById(1),
-//			$this->orm->videos->getById(3),
-//			$this->orm->blueprints->getById(1),
-//			$this->orm->videos->getById(2),
-//			$this->orm->videos->getById(5),
-//		];
-//		//
-//		$this->orm->paths->attach($p);
-//		$p->getRepository()->getMapper(); // HACK, move events to repository
-//		$this->orm->flush();
-
-//		$path = $this->orm->paths->getById(1);
-//		$path->list = [
-//			$this->orm->videos->getById(4),
-//			$this->orm->videos->getById(3),
-//			$this->orm->blueprints->getById(1),
-//			$this->orm->videos->getById(2),
-//			$this->orm->videos->getById(5),
-//		];
-//		$this->orm->flush();
-
 		$this->template->video = $this->orm->videos->getById(3);
 
 		$gist = $this->orm->gists->getByName('test');
