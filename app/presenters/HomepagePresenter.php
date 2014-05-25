7f7ac180041541e3da9eb26eff235d1eae73a66d
Mikulas <rullaf@gmail.com>
2014-05-25 10:36:50 +0200

    fix homepage gist renderer

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -8,15 +8,19 @@ use App\Components\GistRenderer;
 final class HomepagePresenter extends BasePresenter
 {
 
+	public function createComponentGist()
+	{
+		$gist = $this->orm->gists->getByName('test');
+		$gistRenderer = parent::createComponentGist();
+		$gistRenderer->setEditable(TRUE);
+		$gistRenderer->setGist($gist);
+
+		return $gistRenderer;
+	}
+
 	public function renderDefault()
 	{
 		$this->template->video = $this->orm->videos->getById(3);
-
-		$gist = $this->orm->gists->getByName('test');
-		/** @var GistRenderer $gistRenderer */
-		$gistRenderer = $this['gist'];
-		$gistRenderer->setEditable(TRUE);
-		$gistRenderer->setGist($gist);
 	}
 
 }
