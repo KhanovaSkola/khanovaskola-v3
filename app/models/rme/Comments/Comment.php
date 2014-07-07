8141ee2404fedbc4cfd196fdad7665fe238a185c
Mikulas <rullaf@gmail.com>
2014-07-07 14:35:17 +0200

    fix comments entity properties

--- app/models/rme/Comments/Comment.php
+++ app/models/rme/Comments/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Rme;
 
+use App;
 use App\Models\Orm\Entity;
 use Orm;
 
@@ -9,10 +10,10 @@ use Orm;
 /**
  * @property string $text
  *
- * @property NULL|Comment $inReplyTo {m:1 comments $replies}
+ * @property NULL|App\Models\Rme\Comment $inReplyTo {m:1 comments $replies}
  * @property Orm\OneToMany $replies {1:m comments $inReplyTo}
- * @property User $author {m:1 users $comments}
- * @property Video $video {m:1 videos $comments}
+ * @property App\Models\Rme\User $author {m:1 users $comments}
+ * @property App\Models\Rme\Video $video {m:1 videos $comments}
  */
 class Comment extends Entity
 {
