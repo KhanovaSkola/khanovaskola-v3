c33ce8fc16b34dc522a5c88e70db4193913c3204
Mikulas <rullaf@gmail.com>
2014-07-07 11:53:27 +0200

    [dev] fix travis

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-set -e # exit on first erorr
-
-if [ "$TEST_SUITE" = "cs" ]
-then
-	exit 0
-fi
+set -e # exit on first error
 
 block() {
 	php -r "echo \"\n\e[1;30;44m$1\e[21m\n\";"
@@ -33,23 +28,25 @@ beanstalkd -l 127.0.0.1 &
 
 block "Setting configuration"
 cat tests/travis/php-custom.ini >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-rm tests/acceptance.suite.yml
-mv tests/travis/acceptance.suite.yml tests/acceptance.suite.yml
 cp tests/travis/config.local.neon app/config/config.local.neon
 php tests/travis/decrypt.php
-
-if [ "$TEST_SUITE" = "acceptance" ]
-then
-	block "Starting local server"
-	php -S travis.khanovaskola.cz:8000 -t www/ &
-fi
 
 block "Waiting for all services to load"
 sleep 10 # wait for all services to load
 curl "http://localhost:9200/" # check elastic search
 
+if [ "$TEST_SUITE" = "acceptance" ]
+then
+	block "Installing phantomjs, casperjs"
+	npm install -g phantomjs
+	npm install -g casperjs
+
+	block "Starting local server"
+	php -S travis.khanovaskola.cz:80 -t www/ &
+fi
+
 block "Provisioning"
 psql -c 'create database khanovaskola;' -U postgres
-bin/console db:migrate
-bin/console es:recreate
-bin/console db:fill
+php www/index.php m:m -i
+php www/index.php m:m
+php www/index.php elastic:recreate
--- tests/travis/test.sh
+++ tests/travis/test.sh
@@ -2,15 +2,15 @@
 if [ "$TEST_SUITE" = "unit" ]
 then
 	php -r 'echo "\n\n\e[1;30;44m Running UNIT tests \e[21m\n\n";'
-	php vendor/bin/tester -c ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini tests/unit/
+	php vendor/bin/tester -c ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini tests/cases/unit/
 elif [ "$TEST_SUITE" = "acceptance" ]
 then
 	php -r 'echo "\n\n\e[1;30;44m Running ACCEPTANCE tests \e[21m\n\n";'
-	php vendor/bin/codecept run acceptance
+	php www/index.php tests:run --cept
 elif [ "$TEST_SUITE" = "cs" ]
 then
 	php -r 'echo "\n\n\e[1;30;44m Running CS tests \e[21m\n\n";'
-	sh tests/cs/run.sh
+	php www/index.php tests:run --cs
 else
 	php -r 'echo "\n\n\e[1;30;41m Unknown TEST_SUITE \e[21m\n\n";'
 	exit 1
