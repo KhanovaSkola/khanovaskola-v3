332aaeb3e4caad16b7cf7f6e1dbf42435707064f
Mikulas <rullaf@gmail.com>
2014-07-07 11:53:27 +0200

    [dev] fix travis

--- tests/travis/success.sh
+++ tests/travis/success.sh
@@ -1,6 +1,1 @@
 #!/bin/sh
-if [ "$TEST_SUITE" = "cs" ]
-then
-	php -r 'echo "\n\n\e[1;30;44m Running additional CS tests \e[21m\n\n";'
-	php vendor/bin/phpcs -p --severity=1 --standard=tests/cs app
-fi
