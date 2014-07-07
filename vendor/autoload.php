272b3bb5e166b850f70a561e480363c8fc05d339
Mikulas <rullaf@gmail.com>
2014-07-07 16:51:47 +0200

    [dev] update query panel

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitdbf4de1bf5d5a9cdbed0bfbe9df3f4fc::getLoader();
+return ComposerAutoloaderInita223548eb20cf79c58757a67964464e2::getLoader();
