3cd71623a0903a0b64a1f9a20172ee679b3e4830
Mikulas <rullaf@gmail.com>
2014-07-07 10:30:58 +0200

    change dev dependencies
    Installing doctrine/annotations (v1.2.0)
    Installing mikulas/code-sniffs (v1.1.0)
    
    Removing codeception/codeception (1.8.6)
    Removing symfony/browser-kit (v2.5.0)
    Removing symfony/css-selector (v2.5.0)
    Removing symfony/finder (v2.5.0)
    Removing symfony/dom-crawler (v2.5.0)
    Removing fabpot/goutte (v1.0.6)
    Removing phpunit/phpunit (3.7.37)
    Removing facebook/webdriver (v0.3)
    Removing phpunit/php-code-coverage (1.2.17)
    Removing phpunit/php-file-iterator (1.3.4)
    Removing phpunit/php-token-stream (1.2.2)
    Removing phpunit/php-timer (1.0.5)
    Removing phpunit/phpunit-mock-objects (1.2.3)
    Removing phpunit/php-text-template (1.2.0)
    Removing behat/mink-goutte-driver (v1.0.9)
    Removing behat/mink-browserkit-driver (v1.1.0)
    Removing behat/mink-selenium2-driver (v1.1.1)
    Removing behat/mink (v1.5.0)
    Removing instaclick/php-webdriver (1.0.17)
    Removing phpseclib/phpseclib (0.3.6)
    Removing elfet/deployer (dev-master b430b8a)
    Removing doctrine/annotations (v1.1.2)

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -8,9 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     $vendorDir . '/tracy/tracy/src/shortcuts.php',
     $vendorDir . '/nette/nette/Nette/loader.php',
-    $vendorDir . '/phpseclib/phpseclib/phpseclib/Crypt/Random.php',
     $vendorDir . '/kdyby/events/src/Doctrine/compatibility.php',
     $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
-    $vendorDir . '/elfet/deployer/src/deployer.php',
     $vendorDir . '/petrp/access/Access/Init.php',
 );
