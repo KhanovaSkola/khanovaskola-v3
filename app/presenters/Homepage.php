0e66601a0cded3c5a2f41cb327ffa340af3c0add
Mikulas <rullaf@gmail.com>
2014-07-08 12:46:36 +0200

    remove homepage gist, add user links to layout

--- app/presenters/Homepage.php
+++ app/presenters/Homepage.php
@@ -2,27 +2,8 @@
 
 namespace App\Presenters;
 
-use App\Components\Controls\GistRenderer;
-
 
 final class Homepage extends Presenter
 {
-
-	public function createComponentGist()
-	{
-		$gist = $this->orm->gists->getByName('test');
-
-		/** @var GistRenderer $gistRenderer */
-		$gistRenderer = $this->buildComponent(GistRenderer::class);
-		$gistRenderer->setEditable(TRUE);
-		$gistRenderer->setGist($gist);
-
-		return $gistRenderer;
-	}
-
-	public function renderDefault()
-	{
-		$this->template->video = $this->orm->videos->getById(3);
-	}
 
 }
