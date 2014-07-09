f00e6e7d5271a4561555cc5cd501176043267ca4
Mikulas <rullaf@gmail.com>
2014-07-09 12:58:12 +0200

    update videoView

--- app/models/rme/VideoViews/VideoView.php
+++ app/models/rme/VideoViews/VideoView.php
@@ -9,8 +9,25 @@ use App\Models\Orm\Entity;
 /**
  * @property App\Models\Rme\Video $video {m:1 videos $views}
  * @property App\Models\Rme\User $user {m:1 users $videosViewed}
+ * @property float $percent [0..100]
+ * @property float $time seconds
+ * @property float $furthest seconds, max time seen (eg if user jumps to second 5, this will be 5)
+ * @property string $flow json
  */
 class VideoView extends Entity
 {
+
+	/**
+	 * @param float $percent
+	 */
+	public function setPercent($percent)
+	{
+		if ($percent < 0 || $percent > 100)
+		{
+			throw new App\InvalidArgumentException;
+		}
+
+		$this->setValue('percent', $percent);
+	}
 
 }
