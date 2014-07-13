a8d021c067a5251750c7f5d180e69ab84d5c972b
Mikulas <rullaf@gmail.com>
2014-07-13 11:08:25 +0200

    [test] update dummy cept test

---
+++ tests/cases/cept/homepage-has-search.js
@@ -0,0 +1,17 @@
+casper.test.begin('Homepage has search and search term is present on results page', 2, function suite(test) {
+	casper.start("http://travis.khanovaskola.cz", function() {
+		var form = 'form[id="frm-search-form"]';
+		test.assertExists(form, "Search form is found");
+		this.fill(form, {
+			query: "rychlost"
+		}, true);
+	});
+
+	casper.waitForUrl(/results/, function() {
+		test.assertTextExists("Výsledky pro rychlost");
+	});
+
+	casper.run(function() {
+		test.done();
+	});
+});
