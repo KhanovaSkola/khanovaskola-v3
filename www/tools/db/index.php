00c13fb7815fdbb16edc21c55d51d10a4b552a6e
Mikulas <rullaf@gmail.com>
2014-09-05 11:28:08 +0200

    [dev] add adminer wrapper

--- www/tools/db/index.php
+++ www/tools/db/index.php
@@ -1,3 +1,25 @@
 <?php
 
+function adminer_object()
+{
+
+	class AdminerSoftware extends Adminer
+	{
+
+		public function name()
+		{
+			return 'Khanova škola';
+		}
+
+		public function database()
+		{
+			// database name, will be escaped by Adminer
+			return 'khanovaskola';
+		}
+
+	}
+
+	return new AdminerSoftware;
+}
+
 require __DIR__ . '/../../../vendor/mikulas/adminer-package/adminer.php';
