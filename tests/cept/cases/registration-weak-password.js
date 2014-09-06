ec54b97bbe1073d8ba39e01a9165d01cb5907bb1
Mikulas <rullaf@gmail.com>
2014-09-06 23:19:59 +0200

    [test] rewrite cept with router link

--- tests/cept/cases/registration-weak-password.js
+++ tests/cept/cases/registration-weak-password.js
@@ -1,34 +1,34 @@
 casper.test.begin('Warn when registering with weak password', 5, function suite(test) {
-	casper.echo('casper.start');
-	casper.start("//Auth:registration", function() {
-		casper.echo('casper.start callback');
-		var form = 'form[id="frm-registrationForm-form"]';
-		test.assertExists(form, "Registration form is found");
+	casper.plink("//Auth:registration", {}, function(url) {
+		casper.start(url, function() {
+			var form = 'form[id="frm-registrationForm-form"]';
+			test.assertExists(form, "Registration form is found");
 
-		this.fill(form, {
-			password: "weak"
-		}, false);
-		test.assertExists('.has-warning [name="password"]', "Warn on weak password");
+			this.fill(form, {
+				password: "weak"
+			}, false);
+			test.assertExists('.has-warning [name="password"]', "Warn on weak password");
 
-		this.fill(form, {
-			password: "ifPQu4qcjh9oT$Rg[PsBWw[m9"
-		}, false);
-		test.assertDoesntExist('.has-warning [name="password"]', "Approve strong password");
+			this.fill(form, {
+				password: "ifPQu4qcjh9oT$Rg[PsBWw[m9"
+			}, false);
+			test.assertDoesntExist('.has-warning [name="password"]', "Approve strong password");
 
-		this.fill(form, {
-			email: 'ifPQu4qcjh9oT$Rg[PsBWw[m9@gmail.com',
-			password: "ifPQu4qcjh9oT$Rg[PsBWw[m9"
-		}, false);
-		test.assertExist('.has-warning [name="password"]', "Username in password considered weak");
+			this.fill(form, {
+				email: 'ifPQu4qcjh9oT$Rg[PsBWw[m9@gmail.com',
+				password: "ifPQu4qcjh9oT$Rg[PsBWw[m9"
+			}, false);
+			test.assertExist('.has-warning [name="password"]', "Username in password considered weak");
 
-		this.fill(form, {
-			name: 'Mikuláš Dítě',
-			password: "dite mikulas"
-		}, false);
-		test.assertExist('.has-warning [name="password"]', "Name in password considered weak");
-	});
+			this.fill(form, {
+				name: 'Mikuláš Dítě',
+				password: "dite mikulas"
+			}, false);
+			test.assertExist('.has-warning [name="password"]', "Name in password considered weak");
+		});
 
-	casper.run(function() {
-		test.done();
+		casper.run(function() {
+			test.done();
+		});
 	});
 });
