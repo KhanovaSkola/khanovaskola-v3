247f2edb7bf9a3192776e91c14aabb4d4ea5f377
Mikulas <rullaf@gmail.com>
2014-09-06 23:07:02 +0200

    [dev] add link command

---
+++ bin/commands/Link.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bin\Commands;
+
+use App\Presenters\Homepage;
+use Nette\Application\PresenterFactory;
+use Symfony\Component\Console\Input\InputArgument;
+
+
+class Link extends Command
+{
+
+	protected function configure()
+	{
+		$this->setName('link')
+			->setDescription('Generate url from router path')
+			->addArgument('target', InputArgument::REQUIRED, 'such as Homepage:profile or //Auth:in');
+	}
+
+	public function invoke(PresenterFactory $fact)
+	{
+		/** @var Homepage $presenter */
+		$presenter = $fact->createPresenter('Homepage');
+		$args = [];
+		foreach ($this->in->getVariadics() as $part)
+		{
+			list($key, $val) = explode('=', $part);
+			$args[$key] = $val;
+		}
+		$link = $presenter->link($this->in->getArgument('target'), $args);
+		$this->out->writeln($link);
+	}
+
+}
