25c740ad83e6463a4aa5d975312930ac942be053
Mikulas <rullaf@gmail.com>
2014-09-09 19:53:38 +0200

    add content type

--- app/models/rme/Contents/ContentsRepository.php
+++ app/models/rme/Contents/ContentsRepository.php
@@ -12,4 +12,32 @@ use Orm\DibiCollection;
 class ContentsRepository extends Repository
 {
 
+	public static function getClasses()
+	{
+		return [
+			'blueprint' => Blueprint::class,
+			'video' => Video::class,
+		];
+	}
+
+	public static function getTypes()
+	{
+		return array_keys(static::getClasses());
+	}
+
+	/**
+	 * @param array $data
+	 * @return array|string
+	 */
+	public function getEntityClassName(array $data = NULL)
+	{
+		$classes = static::getClasses();
+		if ($data === NULL)
+		{
+			return array_values($classes);
+		}
+
+		return $classes[$data['type']];
+	}
+
 }
