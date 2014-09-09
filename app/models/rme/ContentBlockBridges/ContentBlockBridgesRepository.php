525e91f33bb79aa482dff3c9e2959d311cc2dd07
Mikulas <rullaf@gmail.com>
2014-09-09 16:54:29 +0200

    add contentBlockBridge

---
+++ app/models/rme/ContentBlockBridges/ContentBlockBridgesRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Rme;
+
+use App\Models\Orm\Repository;
+use Orm\DibiCollection;
+
+
+/**
+ * @method DibiCollection|ContentBlockBridge[] findAll()
+ */
+class ContentBlockBridgesRepository extends Repository
+{
+
+}
