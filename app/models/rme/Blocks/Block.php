1b03612ebdc43c6bf1e8f0ca87eefcc275f15bb7
Mikulas <rullaf@gmail.com>
2014-09-09 14:31:03 +0200

    add link blocks and schemas to rel db

--- app/models/rme/Blocks/Block.php
+++ app/models/rme/Blocks/Block.php
@@ -4,13 +4,14 @@ namespace App\Models\Rme;
 
 use App\Models\Orm\ContentEntity;
 use App\Models\Orm\Entity;
+use Orm\ManyToMany as MtM;
 
 
 /**
- * @property string                $name
- * @property ContentEntity[]|array $list
- * @property Schema[]|array        $schemas
- * @property User                  $author  {m:1 users $blocksAuthored}
+ * @property string          $name
+ * @property ContentEntity[] $list
+ * @property MtM|Schema[]    $schemas {m:m schemas $blocks}
+ * @property User            $author  {m:1 users $blocksAuthored}
  */
 class Block extends Entity
 {
