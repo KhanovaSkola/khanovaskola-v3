763b965c910673e547ac7537050e7d156b754440
Mikulas <rullaf@gmail.com>
2014-09-09 14:31:03 +0200

    add link blocks and schemas to rel db

--- app/models/rme/Schemas/Schema.php
+++ app/models/rme/Schemas/Schema.php
@@ -3,12 +3,13 @@
 namespace App\Models\Rme;
 
 use App\Models\Orm\Entity;
+use Orm\ManyToMany as MtM;
 
 
 /**
- * @property string  $name
- * @property Block[] $blocks {ignore} {m:m blocks $schemas} mapped in neo4j
- * @property User    $author {m:1 users $schemasAuthored}
+ * @property string      $name
+ * @property MtM|Block[] $blocks {m:m blocks $schemas mapped}
+ * @property User        $author {m:1 users $schemasAuthored}
  */
 class Schema extends Entity
 {
