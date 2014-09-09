c547a2e96ab2981b4430e3d4fa6125de10c6ffa0
Mikulas <rullaf@gmail.com>
2014-09-09 16:54:14 +0200

    add contents

---
+++ app/models/rme/Contents/ContentsRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models\Rme;
+
+use App\Models\Orm\Repository;
+use Orm\DibiCollection;
+
+
+/**
+ * @method DibiCollection|Content[] findAll()
+ */
+class ContentsRepository extends Repository
+{
+
+}
