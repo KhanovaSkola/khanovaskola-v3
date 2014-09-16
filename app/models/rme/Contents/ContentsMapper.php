e76966efcddb7d32b7d42a0268af35f853f944e5
Mikulas <rullaf@gmail.com>
2014-09-16 18:22:35 +0200

    refactor json mapping out of contents mapper

--- app/models/rme/Contents/ContentsMapper.php
+++ app/models/rme/Contents/ContentsMapper.php
@@ -3,38 +3,14 @@
 namespace App\Models\Rme;
 
 use App\Models\Orm\Mappers\Mapper;
-use Orm\EventArguments;
-use Orm\Events;
 
 
 class ContentsMapper extends Mapper
 {
 
-	public function registerEvents(Events $events)
+	public function getJsonFields()
 	{
-		parent::registerEvents($events);
-
-		$keys = ['vars', 'hints'];
-		$events->addCallbackListener($events::HYDRATE_BEFORE, function (EventArguments $args) use ($keys)
-		{
-			foreach ($keys as $key)
-			{
-				if (isset($args->data[$key]))
-				{
-					$args->data[$key] = json_decode($args->data[$key]);
-				}
-			}
-		});
-		$events->addCallbackListener($events::SERIALIZE_BEFORE, function (EventArguments $args) use ($keys)
-		{
-			foreach ($keys as $key)
-			{
-				if (isset($args->values[$key]))
-				{
-					$args->values[$key] = json_encode($args->values[$key]);
-				}
-			}
-		});
+		return ['vars', 'hints'];
 	}
 
 }
