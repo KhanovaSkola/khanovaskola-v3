c8dc728ee2de4bdf08246ef4a7041a50888330aa
Mikulas <rullaf@gmail.com>
2014-09-23 23:16:18 +0200

    add password entropy service

---
+++ app/models/services/Entropy.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\Services;
+
+use App\Models\Rme\User;
+use Nette\Utils\Strings;
+use ZxcvbnPhp\Zxcvbn;
+
+
+class Entropy
+{
+
+	/**
+	 * @var Zxcvbn
+	 */
+	protected $computer;
+
+	public function __construct()
+	{
+		$this->computer = new Zxcvbn();
+	}
+
+	public function compute($password, User $user)
+	{
+		$fields = ['name', 'email', 'familyName', 'nominative', 'vocative'];
+		$parts = [];
+		$index = 1;
+		foreach ($fields as $field)
+		{
+			foreach (preg_split('~[@._\s+]~', $user->$field) as $part)
+			{
+				$parts[$part] = $index++;
+				$parts[Strings::toAscii($part)] = $index++;
+			}
+		}
+
+		try
+		{
+			$result = $this->computer->passwordStrength($password, $parts);
+		}
+		catch (\Exception $e)
+		{
+			return NULL;
+		}
+
+		return number_format($result['entropy'], 2);
+	}
+
+}
