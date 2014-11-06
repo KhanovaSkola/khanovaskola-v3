3793ba832cd08fe50f085c4954f6dddebe6aa4e3
Mikulas <rullaf@gmail.com>
2014-11-06 18:19:29 +0100

    fix header border disappearing

--- www/js/components/header.js
+++ www/js/components/header.js
@@ -3,6 +3,7 @@ $(function() {
 	$('.dropdown-big .dropdown-toggle').on('click', function() {
 		var $dropdown = $(this).parents('.dropdown').first();
 		$dropdown.toggleClass('open');
+		$dropdown.parents('header').toggleClass('hover');
 	});
 
 	$('.ul-tab a').click(function (e) {
