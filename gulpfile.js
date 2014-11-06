005e870854801f4373c58621c2c20912bc49fae7
Mikulas <rullaf@gmail.com>
2014-11-06 16:21:47 +0100

    add new frontend

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var path = require('path');
+
+var lessDir = './less';
+var lessFiles = path.join(lessDir, '*', '**');
+var lessMainFile = path.join(lessDir, 'main.less');
+var cssDir = './css';
+
+gulp.task('less', function () {
+	gulp.src(lessMainFile)
+		.pipe($.sourcemaps.init())
+		.pipe($.less())
+		.pipe($.autoprefixer())
+		.pipe($.rename({suffix: '.min'}))
+		.pipe($.sourcemaps.write('.', {
+			sourceRoot: '../less'
+		}))
+		.pipe(gulp.dest(cssDir))
+		.pipe($.livereload());
+});
+
+gulp.task('production', function () {
+	gulp.src(lessMainFile)
+		.pipe($.less())
+		.pipe($.autoprefixer())
+		.pipe($.cssmin())
+		.pipe($.rename({suffix: '.min'}))
+		.pipe(gulp.dest(cssDir));
+});
+
+gulp.task('default', function() {
+	gulp.start('less');
+});
+
+gulp.task('watch', ['less'], function() {
+	//Watch changes (less, )
+	$.watch('less/**.less', ['less']);
+	$.watch('less/**/**.less', ['less']);
+	$.watch('*.html', ['less']);
+});
