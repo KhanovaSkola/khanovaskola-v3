a9a35285aef0a3275df2ba53536a8bae1e2a4d2b
Mikulas <rullaf@gmail.com>
2014-11-16 11:44:16 +0100

    simplify block and schema api

--- app/models/rme/Schemas/Schema.php
+++ app/models/rme/Schemas/Schema.php
@@ -11,8 +11,21 @@ use Orm\OneToMany as OtM;
  * @property OtM|BlockSchemaBridge[] $blockSchemaBridges {1:m blockSchemaBridges $schema}
  * @property Subject                 $subject            {m:1 subjects $schemas}
  * @property User                    $author             {m:1 users $schemasAuthored}
+ *
+ * @property-read Block[] $blocks
  */
 class Schema extends Entity
 {
+
+	/**
+	 * @return Block[]
+	 */
+	public function getBlocks()
+	{
+		foreach ($this->blockSchemaBridges as $bridge)
+		{
+			yield $bridge->block;
+		}
+	}
 
 }
