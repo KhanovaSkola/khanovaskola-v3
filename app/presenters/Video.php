702aaf57c86504adaece12129409787a1c716460
Mikulas <rullaf@gmail.com>
2014-11-17 15:13:47 +0100

    add block and schema to all links

--- app/presenters/Video.php
+++ app/presenters/Video.php
@@ -10,17 +10,28 @@ use App\Presenters\Parameters;
 final class Video extends Content
 {
 
+	use Parameters\Block;
+	use Parameters\Schema;
 	use Parameters\Video;
 
 	public function startup()
 	{
 		parent::startup();
+		$this->loadSchema();
+		$this->loadBlock();
 		$this->loadVideo();
+
+		if (!$this->schema->contains($this->block) || !$this->block->contains($this->video))
+		{
+			$this->error();
+		}
 	}
 
 	public function renderDefault()
 	{
 		$this->template->video = $this->video;
+		$this->template->block = $this->block;
+		$this->template->schema = $this->schema;
 		$this->template->suggestions = $this->getSuggestions($this->video);
 	}
 
