fca2570989085b64c31b9811da0e211d1431d6c6
Mikulas <rullaf@gmail.com>
2014-11-17 17:36:02 +0100

    fix blueprint editor

--- app/presenters/Parameters/Blueprint.php
+++ app/presenters/Parameters/Blueprint.php
@@ -20,13 +20,17 @@ trait Blueprint
 	 */
 	protected $blueprint;
 
-	protected function loadBlueprint()
+	protected function loadBlueprint(callable $fallback = NULL)
 	{
 		/** @var Presenter $this */
 		$this->blueprint = $this->orm->contents->getById($this->blueprintId);
 		if (!$this->blueprint || ! $this->blueprint instanceof Rme\Blueprint)
 		{
-			$this->error();
+			if (!$fallback)
+			{
+				$this->error();
+			}
+			$this->blueprint = $fallback();
 		}
 	}
 
