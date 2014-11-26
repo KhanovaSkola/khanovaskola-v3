7533960b9408ac3546851943d49ec30321928a47
Mikulas <rullaf@gmail.com>
2014-11-26 17:59:22 +0100

    add youtube preview service

---
+++ app/models/services/YoutubePreview.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\Services;
+
+
+class YoutubePreview
+{
+
+	/**
+	 * @see http://stackoverflow.com/a/20542029/326257
+	 * @param $youtubeId
+	 * @return NULL|string
+	 */
+	public function getImageUrlFor($youtubeId)
+	{
+		foreach ([
+				'http://img.youtube.com/vi/%s/maxresdefault.jpg', // 1920x1080
+				'http://img.youtube.com/vi/%s/sddefault.jpg', // 640x480
+				'http://img.youtube.com/vi/%s/hqdefault.jpg', // 480x360
+			] as $template)
+		{
+			$url = sprintf($template, $youtubeId);
+			$headers = get_headers($url, TRUE);
+			if (strpos($headers[0], '200 OK') !== FALSE)
+			{
+				return $url;
+			}
+		}
+
+		return NULL;
+	}
+
+}
