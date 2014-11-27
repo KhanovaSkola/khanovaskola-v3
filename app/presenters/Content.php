6b372ea6b5a4e7ddedbf2e129349b5580aa64b41
Mikulas <rullaf@gmail.com>
2014-11-27 17:29:58 +0100

    remove path finder service

--- app/presenters/Content.php
+++ app/presenters/Content.php
@@ -3,24 +3,18 @@
 namespace App\Presenters;
 
 use App\Models\Rme;
-use App\Models\Services\PathFinder;
 
 
 abstract class Content extends Presenter
 {
 
-	/**
-	 * @var PathFinder
-	 * @inject
-	 */
-	public $pathFinder;
-
 	public function getSuggestions(Rme\Content $entity)
 	{
-		$section = $this->session->getSection('paths');
-		$section->steps = [4, 4]; // TODO save automatically all paths this video is part of
+//		$section = $this->session->getSection('paths');
+//		$section->steps = [4, 4]; // TODO save automatically all paths this video is part of
 
-		return $this->pathFinder->suggestNext($entity);
+//		return $this->pathFinder->suggestNext($entity);
+		return [];
 	}
 
 }
