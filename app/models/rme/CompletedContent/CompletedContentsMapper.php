a95bdd6287ee8479b3836c8d2dbe483eb3dada48
Mikulas <rullaf@gmail.com>
2014-12-05 15:35:08 +0100

    add completedContents method for last schemas

--- app/models/rme/CompletedContent/CompletedContentsMapper.php
+++ app/models/rme/CompletedContent/CompletedContentsMapper.php
@@ -8,4 +8,25 @@ use App\Models\Orm\Mappers\Mapper;
 class CompletedContentsMapper extends Mapper
 {
 
+	/**
+	 * @param User $user
+	 * @return Schema[]
+	 */
+	public function getLatestSchemas(User $user)
+	{
+		$schemas = [];
+
+		/** @var CompletedContent $completed */
+		foreach ($this->findBy(['user_id' => $user->id])->orderBy('createdAt', 'DESC') as $completed)
+		{
+			$schema = $completed->schema;
+			if ($schema && !isset($schemas[$schema->id]))
+			{
+				$schemas[$schema->id] = $schema;
+			}
+		}
+
+		return $schemas;
+	}
+
 }
