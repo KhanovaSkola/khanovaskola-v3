7bd3cff254b3f0b4b326bae66cfd12dc45bf2903
Mikulas <rullaf@gmail.com>
2014-12-10 20:47:13 +0100

    add subject, schema, block editors

--- app/models/rme/Subjects/Subject.php
+++ app/models/rme/Subjects/Subject.php
@@ -3,6 +3,7 @@
 namespace App\Models\Rme;
 
 use App\Models\Orm\TitledEntity;
+use Orm\ManyToMany as MtM;
 use Orm\OneToMany as OtM;
 
 
@@ -10,6 +11,7 @@ use Orm\OneToMany as OtM;
  * @property OtM|Schema[] $schemas {1:m schemas $subject}
  * @property string       $color hex
  * @property string       $icon
+ * @property MtM|User[]   $editors {m:m users $subjectsEdited}
  */
 class Subject extends TitledEntity
 {
--- app/models/rme/Users/UsersMapper.php
+++ app/models/rme/Users/UsersMapper.php
@@ -3,6 +3,8 @@
 namespace App\Models\Rme;
 
 use App\Models\Orm\Mappers;
+use Orm\DibiManyToManyMapper;
+use Orm\IRepository;
 
 
 class UsersMapper extends Mappers\Mapper
@@ -28,6 +30,33 @@ class UsersMapper extends Mappers\Mapper
 				[nominative] = %s
 		', $gender, $name)->fetchSingle();
 		return explode(':', $dat)[0];
+	}
+
+	public function createManyToManyMapper($param, IRepository $targetRepository, $targetParam)
+	{
+		/** @var DibiManyToManyMapper $mtm */
+		$mtm = parent::createManyToManyMapper($param, $targetRepository, $targetParam);
+
+		if ($targetRepository instanceof SubjectsRepository)
+		{
+			$mtm->table = 'editors_x_subjects';
+			$mtm->parentParam = 'user_id';
+			$mtm->childParam = 'subject_id';
+		}
+		else if ($targetRepository instanceof SchemasRepository)
+		{
+			$mtm->table = 'editors_x_schemas';
+			$mtm->parentParam = 'user_id';
+			$mtm->childParam = 'schema_id';
+		}
+		else if ($targetRepository instanceof BlocksRepository)
+		{
+			$mtm->table = 'editors_x_blocks';
+			$mtm->parentParam = 'user_id';
+			$mtm->childParam = 'block_id';
+		}
+
+		return $mtm;
 	}
 
 }
