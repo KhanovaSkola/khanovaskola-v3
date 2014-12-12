245eec4d357d4e6feeb21254e12dd05e4126f6de
Mikulas <rullaf@gmail.com>
2014-12-12 15:52:47 +0100

    add block editor

---
+++ app/presenters/BlockEditor.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Presenters;
+
+use App\Models\Rme;
+use App\Presenters\Parameters;
+use Nette\Forms\Controls\TextInput;
+
+
+class BlockEditor extends Presenter
+{
+
+	use Parameters\Block;
+	use Parameters\Schema;
+
+	public function startup()
+	{
+		parent::startup();
+
+		$this->loadBlock(function() {
+			return NULL;
+		});
+		$this->loadSchema(function() {
+			return NULL;
+		});
+
+		if ($this->block && $this->schema && !$this->schema->contains($this->block))
+		{
+			$this->error();
+		}
+
+		if ($this->block && !$this->user->isAllowed($this->block))
+		{
+			$this->flashError('acl.denied.block');
+			$this->redirect('Homepage:default');
+		}
+	}
+
+	public function renderDefault()
+	{
+		$this->template->add('block', $this->block);
+		$this->template->add('schema', $this->schema);
+
+		if ($this->block)
+		{
+			/** @var self|TextInput[] $this */
+			$this['blockForm-form-title']->setDefaultValue($this->block->title);
+			$this['blockForm-form-description']->setDefaultValue($this->block->description);
+		}
+	}
+
+}
