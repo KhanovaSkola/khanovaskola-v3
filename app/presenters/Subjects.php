63455463f7c4c4c3732367572f30b6c8a1b7f4bf
Mikulas <rullaf@gmail.com>
2014-12-19 18:33:55 +0200

    fix subjects page layout

--- app/presenters/Subjects.php
+++ app/presenters/Subjects.php
@@ -9,6 +9,25 @@ final class Subjects extends Presenter
 	public function renderDefault()
 	{
 		$this->setCacheControlPublic();
+
+		// group by 5 for easier rendering
+		$groups = [];
+		$group = [];
+		foreach ($this->orm->subjects->findAll() as $subject)
+		{
+			$group[] = $subject;
+			if (count($group) === 5)
+			{
+				$groups[] = $group;
+				$group = [];
+			}
+		}
+		if ($group)
+		{
+			$groups[] = $group;
+		}
+
+		$this->template->groupedSubjects = $groups;
 	}
 
 }
