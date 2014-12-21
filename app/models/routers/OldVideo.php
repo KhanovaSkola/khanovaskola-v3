657838b5a5ac2ce7903e66a2b34dcb1a4154839b
Mikulas <rullaf@gmail.com>
2014-12-21 11:09:17 +0200

    add old links routers

---
+++ app/models/routers/OldVideo.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models\Routers;
+
+use Nette\Application\Request;
+use Nette\Http;
+use Nette\Utils\Strings;
+
+
+class OldVideo extends OldLinkRouter
+{
+
+	/**
+	 * Maps HTTP request to a Request object.
+	 *
+	 * @param Http\IRequest $httpRequest
+	 * @return NULL|Request
+	 */
+	public function match(Http\IRequest $httpRequest)
+	{
+		$path = ltrim($httpRequest->getUrl()->getPath(), '/');
+		$match = Strings::match($path, '~^([^/]+)/(?P<slug>[^/]+)/lekce~');
+		if (!$match)
+		{
+			return NULL;
+		}
+
+		$id = $this->dibi->query('
+			SELECT [c.id]
+			FROM [old_links] [l]
+
+			INNER JOIN [contents] [c] ON [c.youtube_id] = [l.target]
+
+			WHERE [mask] = %s', $match['slug'], '
+		')->fetchSingle();
+		if (!$id)
+		{
+			return NULL;
+		}
+
+		return new Request('Video:default', $httpRequest->getMethod(), [
+			'videoId' => $id,
+		]);
+	}
+
+}
