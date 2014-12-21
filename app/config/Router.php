13d1b8952d581d9e74a2987423586f0a4527836c
Mikulas <rullaf@gmail.com>
2014-12-21 16:14:54 +0200

    add old static links redirects

--- app/config/Router.php
+++ app/config/Router.php
@@ -3,6 +3,7 @@
 namespace App\Config;
 
 use App\Models\Routers;
+use App\Models\Routers\Redirect;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
 use Nette\DI\Container;
@@ -29,7 +30,13 @@ class Router extends RouteList
 		$this[] = new Route('video/<action>/[[<schemaId>/]<blockId>/]<videoId>', 'Video:default');
 		$this[] = new Route('blueprint/<action>/[[<schemaId>/]<blockId>/]<blueprintId>', 'Blueprint:default');
 
-		// Old Links
+		// old links
+		$this[] = new Redirect('dobrovolnici', 'https://wiki.khanovaskola.cz/doku.php?id=dobrovolnici');
+		$this[] = new Redirect('dobrovolnici/preklad', 'https://wiki.khanovaskola.cz/doku.php?id=jaknato');
+		$this[] = new Redirect('dobrovolnici/pravidla-prekladu', 'https://wiki.khanovaskola.cz/doku.php?id=pravidla');
+		$this[] = new Redirect('o-skole', 'https://wiki.khanovaskola.cz/doku.php?id=start');
+		$this[] = new Redirect('o-skole/projekty', 'https://wiki.khanovaskola.cz/doku.php?id=start');
+		$this[] = new Redirect('kontakt', 'https://wiki.khanovaskola.cz/doku.php?id=tym');
 		$this[] = $context->createInstance(Routers\OldVideo::class);
 		$this[] = $context->createInstance(Routers\OldCategory::class);
 		$this[] = $context->createInstance(Routers\OldBlog::class);
