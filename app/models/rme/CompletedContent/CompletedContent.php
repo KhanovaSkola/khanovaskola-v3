dc0f556ddcd9b0a9122c1e1b6c6ab610c5ac5873
Mikulas <rullaf@gmail.com>
2014-12-24 15:21:20 +0200

    remove not null on block and schema on completions

--- app/models/rme/CompletedContent/CompletedContent.php
+++ app/models/rme/CompletedContent/CompletedContent.php
@@ -6,10 +6,10 @@ use App\Models\Orm\Entity;
 
 
 /**
- * @property Content $content {m:1 contents $completions}
- * @property Block   $block   {m:1 blocks $completedContents}
- * @property Schema  $schema  {m:1 schemas $completedContents}
- * @property User    $user    {m:1 users $completedContents}
+ * @property Content     $content {m:1 contents $completions}
+ * @property NULL|Block  $block   {m:1 blocks $completedContents}
+ * @property NULL|Schema $schema  {m:1 schemas $completedContents}
+ * @property User        $user    {m:1 users $completedContents}
  */
 class CompletedContent extends Entity
 {
