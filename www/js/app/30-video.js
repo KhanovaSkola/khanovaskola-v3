d47d3efb098636f45db8987d1d5645ed03d2cb58
Mikulas <rullaf@gmail.com>
2014-12-24 14:31:38 +0200

    add tracking video hooks

--- www/js/app/30-video.js
+++ www/js/app/30-video.js
@@ -4,10 +4,11 @@ App.video.tickerDelta = 100;
 
 App.video.onInit = [];
 App.video.onPlay = [];
-App.video.onPause = [];
-App.video.onTick = [];
-App.video.onSeek = [];
-App.video.onStop = [];
+App.video.onPause = []; // current time
+App.video.onTick = []; // current time
+App.video.onSeek = []; // current time, from time
+App.video.onStop = []; // current time
+App.video.onChangeView = []; // current time, is fullscreen
 
 App.video.init = function() {
 	window.onYouTubeIframeAPIReady = function() {
@@ -37,15 +38,15 @@ App.video.onPlayerStateChange = function(args) {
 	switch (state) {
 		case 1:
 			App.video.ticker = setInterval(function() {
-				App.callAll(App.video.onTick);
+				App.callAll(App.video.onTick, [App.video.player.getCurrentTime()]);
 			}, App.video.tickerDelta);
 			App.callAll(App.video.onPlay);
 			break;
 		case 2:
-			App.callAll(App.video.onPause);
+			App.callAll(App.video.onPause, [App.video.player.getCurrentTime()]);
 			break;
 		case 0:
-			App.callAll(App.video.onStop);
+			App.callAll(App.video.onStop, [App.video.player.getCurrentTime()]);
 			break;
 		default:
 	}
