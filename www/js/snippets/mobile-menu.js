d2a43f2f52cb824532f02aa6aaa4430a5c353db0
Mikulas <rullaf@gmail.com>
2015-01-08 14:01:48 +0200

    mobile nav add dropdown schemas

--- www/js/snippets/mobile-menu.js
+++ www/js/snippets/mobile-menu.js
@@ -3,11 +3,21 @@ $(function() {
 
 	$('#mobile-nav-append').after($('#mobile-nav-snippet').children()).remove();
 
+	var $subjects = $('#mobile-nav-subjects').clone();
+	$('#mobile-nav-subjects-clone').append($subjects);
+
 	$('.container, main').click(function() {
 		$nav.removeClass('open');
 	});
 	$('.burger').click(function() {
 		$nav.addClass('open');
+		return false;
+	});
+
+	$('.footer-mobile ul.schemas, .mobile-nav ul.schemas').slideUp();
+	$('[data-subject-open]').click(function() {
+		var $open = $(this).siblings('[data-subject="' + $(this).data('subject-open') + '"]');
+		$open.slideToggle();
 		return false;
 	});
 });
