78deadedae1569522700210c9905b898903f3536
Mikulas <rullaf@gmail.com>
2015-01-08 13:04:18 +0200

    move footer esi to header esi

--- www/js/snippets/mobile-menu.js
+++ www/js/snippets/mobile-menu.js
@@ -1,5 +1,8 @@
 $(function() {
 	var $nav = $('.mobile-nav');
+
+	$('#mobile-nav-append').after($('#mobile-nav-snippet').children()).remove();
+
 	$('.container, main').click(function() {
 		$nav.removeClass('open');
 	});
