f4ff8162b02e5075fa781e275197d2147a17a297
Mikulas <rullaf@gmail.com>
2015-01-26 12:50:12 +0100

    [dev] fix migration scaffolding command

--- bin/commands/scaffolding/Migration/Sql.php
+++ bin/commands/scaffolding/Migration/Sql.php
@@ -3,13 +3,8 @@
 namespace Bin\Commands\Scaffolding\Migration;
 
 use Bin\Commands\Scaffolding\Command;
-use Bin\Services\DoctrineFactory;
 use Bin\Services\Scaffolding;
 use Bin\Services\SchemaBuilder;
-use Doctrine\DBAL\Platforms\MySqlPlatform;
-use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
-use Doctrine\DBAL\Schema\Comparator;
-use SqlFormatter;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -25,31 +20,9 @@ class Sql extends Command
 			->addOption('from-diff', 'd', InputOption::VALUE_NONE, 'Fill migration with changes from RMEs in app');
 	}
 
-	public function invoke(Scaffolding $scaffolding, DoctrineFactory $factory, SchemaBuilder $schema)
+	public function invoke(Scaffolding $scaffolding)
 	{
 		$file = $scaffolding->createSqlMigration($this->in->getArgument('note'));
-
-		if ($this->in->getOption('from-diff'))
-		{
-			$current = $factory->create()->getSchemaManager()->createSchema();
-			$target = $schema->create();
-			$sqls = Comparator::compareSchemas($current, $target)->toSql(new PostgreSqlPlatform());
-
-			$parts = [];
-			foreach ($sqls as $sql)
-			{
-				if ($sql === 'DROP TABLE migrations')
-				{
-					continue;
-				}
-				$sql = SqlFormatter::format("$sql;", FALSE);
-				$sql = preg_replace('~[ \t]+$~m', '', $sql);
-				$parts[] = $sql;
-			}
-
-			file_put_contents($file, implode("\n\n", $parts) . "\n");
-		}
-
 		$this->writeCreatedFilesHeader();
 		$this->writeCreatedFile($file);
 	}
