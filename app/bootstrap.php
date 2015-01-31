0161d80a8c2379cf5d42cc176d90c12256760284
Mikulas <rullaf@gmail.com>
2015-01-31 14:28:21 +0100

    remove external require, update debug mode ip

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,19 +2,13 @@
 
 umask(0);
 
-if (file_exists('/srv/apps/majka/Inflect.php'))
-{
-	// production inflection-ext fix
-	require '/srv/apps/majka/Inflect.php';
-}
-
 $loader = require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/config/Configurator.php';
 require __DIR__ . '/shortcuts.php';
 
 $configurator = new App\Config\Configurator;
-$configurator->setDebugMode(FALSE&&php_uname('n') === 'swit.hk.cvut.cz');
-$configurator->enableDebugger(__DIR__ . '/../log', 'rullaf+errors@gmail.com');
+$configurator->setDebugMode(['147.32.98.51']);
+$configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->createRobotLoader()
 	->register();
