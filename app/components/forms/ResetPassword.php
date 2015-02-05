b4c2638953fd6883d5c6d2aa17f8ded47cb4533d
Mikulas <rullaf@gmail.com>
2015-02-05 12:39:14 +0100

    fix reset password messages

--- app/components/forms/ResetPassword.php
+++ app/components/forms/ResetPassword.php
@@ -56,8 +56,8 @@ class ResetPassword extends Form
 		if (!$userEntity || !$userEntity->registered)
 		{
 			$this->iLog('form.resetPassword.fail', ['email' => $v->email]);
-			$this->presenter->flashError('auth.reset.notFound');
-			$this->addError('auth.reset.notFound');
+			$this->presenter->flashError('/auth.reset.notFound');
+			$this->addError('/auth.reset.notFound');
 			return;
 		}
 
