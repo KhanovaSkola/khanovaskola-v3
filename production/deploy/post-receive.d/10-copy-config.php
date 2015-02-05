07c23e4026a67a196ad107b949c323f8ad1a5083
Mikulas <rullaf@gmail.com>
2015-02-05 14:15:57 +0100

    [dev] deploy copies new config if found

--- production/deploy/post-receive.d/10-copy-config.php
+++ production/deploy/post-receive.d/10-copy-config.php
@@ -1,6 +1,11 @@
 <?php
 
-if (file_exists("/srv/sites/{$branch}.khanovaskola.cz/app/config/config.local.neon"))
+if (file_exists("/srv/sites/{$branch}.khanovaskola.cz/app/config/config.local.new.neon"))
+{
+	echo "Copying new config.local.neon\n";
+	run("cp /srv/sites/{$branch}.khanovaskola.cz/app/config/config.local.new.neon " . escapeshellarg("$path/app/config/config.local.neon"));
+}
+else if (file_exists("/srv/sites/{$branch}.khanovaskola.cz/app/config/config.local.neon"))
 {
 	echo "Copying previous config.local.neon\n";
 	run("cp /srv/sites/{$branch}.khanovaskola.cz/app/config/config.local.neon " . escapeshellarg("$path/app/config/"));
