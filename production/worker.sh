1d072a48a5d704b9514f93e16d936598d77e634a
Mikulas <rullaf@gmail.com>
2015-02-07 20:46:36 +0100

    fix cron not exiting empty workers

--- production/worker.sh
+++ production/worker.sh
@@ -3,9 +3,14 @@
 # This script is executed every minute by crontab.
 # The tasks exit immediately if idle, otherwise they exit
 # after the task has been completed.
+#
+# If no task ever hit the consumer however, it does not
+# handle the INT signal and we must kill it manually.
+
 
 cd /srv/sites/production.khanovaskola.cz
-timeout --signal=TERM 58 php www/index.php ra:con mail &
-timeout --signal=TERM 58 php www/index.php ra:con updateAvatar &
-timeout --signal=TERM 58 php www/index.php ra:con updateSchema &
-timeout --signal=TERM 58 php www/index.php ra:con updateVideo &
+
+timeout --signal=INT 58 php www/index.php ra:con mail &
+timeout --signal=INT 58 php www/index.php ra:con updateAvatar &
+timeout --signal=INT 58 php www/index.php ra:con updateSchema &
+timeout --signal=INT 58 php www/index.php ra:con updateVideo &
