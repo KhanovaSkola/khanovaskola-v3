b998a68e7cb40a8dc744eb91a21bc8ca6e05b1a7
Mikulas <rullaf@gmail.com>
2015-02-23 11:53:29 +0100

    [js] warn user on low password strength

---
+++ www/jsv2/app/auth/changePassword.js
@@ -0,0 +1,1 @@
+define(['logic/auth/passwordStrength']);
---
+++ www/jsv2/logic/auth/passwordStrength.js
@@ -0,0 +1,23 @@
+define(['services/transliterate', 'zxcvbn'], function(string) {
+	const $form = document.querySelector('[data-password-strength]');
+	const $passwordInput = $form.querySelector('input[name="password"]');
+	const $emailInput = $form.querySelector('input[name="email"]');
+	const $nameInput = $form.querySelector('input[name="name"]');
+
+	const handler = function() {
+		const email = $emailInput.value;
+		const emailTrans = string.transliterate(email);
+		const name = $nameInput.value;
+		const nameTrans = string.transliterate(name);
+
+		// transliterate returns space separated fields
+		const penalize = `${email} ${emailTrans} ${name} ${nameTrans}`.split(' ');
+
+		var score = zxcvbn($passwordInput.value, penalize);
+		$passwordInput.parentNode.classList.toggle('has-warning', score.entropy < 30);
+	};
+
+	$passwordInput.addEventListener('change', handler);
+	$passwordInput.addEventListener('paste', handler);
+	$passwordInput.addEventListener('keyup', handler);
+});
