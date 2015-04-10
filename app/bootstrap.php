8d5e7793b60537d2ee9cb12f954d3ea8ba8f6ee0
Mikulas <rullaf@gmail.com>
2015-04-10 17:39:31 +0200

    change debug url

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,7 +7,7 @@ require __DIR__ . '/config/Configurator.php';
 require __DIR__ . '/shortcuts.php';
 
 $configurator = new App\Config\Configurator;
-$configurator->setDebugMode(['147.32.98.51']);
+$configurator->setDebugMode(['192.168.0.102']);
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->createRobotLoader()
