dba919d64fd89e1db1ffc742c298ed212d80bb61
Mikulas <rullaf@gmail.com>
2015-06-28 11:11:18 +0200

    fix font weight failing on windows without fonts

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ var lessDir = './www/less';
 var lessBootstrapFiles = [
 	path.join(bsDir, 'variables.less'),
 	path.join(lessDir, 'variables.less'),
+	path.join(lessDir, 'fonts.less'),
 
 	path.join(bsDir, 'mixins.less'),
 	path.join(bsDir, 'normalize.less'),
