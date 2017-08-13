d2f7d49b59622b212143a6c906be128cf564df3d
Daniel Hollas <danekhollas@gmail.com>
2017-08-13 23:48:08 +0200

    Optimize sitemap
    Do not include hidden/removed videos and blocks.
    Do not include schemas that are not in subjects.
    Include block.
    Increase relevance of schemas and blocks and main page.

--- app/presenters/Sitemap.php
+++ app/presenters/Sitemap.php
@@ -19,25 +19,36 @@ class Sitemap extends Presenter
 
 			$map = new Cartographer\Sitemap();
 
-			$map->add($this->link('//Auth:registration'), NULL, Freq::YEARLY);
-			$map->add($this->link('//Auth:resetPassword'), NULL, Freq::YEARLY);
-			$map->add($this->link('//Homepage:default'),
-				filemtime(__DIR__ . '/../templates/views/Homepage/default.latte'), Freq::YEARLY);
+			$map->add($this->link('//Auth:registration'), NULL, Freq::YEARLY, 0.1);
+			$map->add($this->link('//Auth:resetPassword'), NULL, Freq::YEARLY, 0.1);
+			$map->add($this->link('//Homepage:default'), NULL, Freq::WEEKLY, 1.0);
+                        // We cannot reliably estimate the time of change, since main menu can change, and we cannot know when
+				//filemtime(__DIR__ . '/../templates/views/Homepage/default.latte'), Freq::MONTHLY,1.0);
+                        //
 			$map->add($this->link('//Text:about'),
-				filemtime(__DIR__ . '/../templates/views/Text/about.latte'), Freq::YEARLY);
+				filemtime(__DIR__ . '/../templates/views/Text/about.latte'), Freq::MONTHLY, 0.7);
+
+		        $map->add('https://blog.khanovaskola.cz/',NULL, Freq::MONTHLY, 0.8);
 
 			foreach ($this->orm->schemas->findAll() as $schema)
 			{
-				$map->add($this->absoluteLink($schema), NULL, Freq::MONTHLY);
+                           if ( ! is_null( $schema->position)) {
+				$map->add($this->absoluteLink($schema), NULL, Freq::MONTHLY, 0.9);
+                           }
 			}
 			foreach ($this->orm->blocks->findAll() as $block)
 			{
-				$map->add($this->absoluteLink($block), NULL, Freq::MONTHLY);
+                           if (! $block->hidden) {
+				$map->add($this->absoluteLink($block), NULL, Freq::MONTHLY, 0.7);
+                           }
 			}
+
 			foreach ($this->orm->contents->findAll() as $content)
 			{
-				$map->add($this->absoluteLink($content), NULL, Freq::YEARLY);
-			}
+                           if (! $content->hidden || ! is_null($content->removedAt) ) { 
+				$map->add($this->absoluteLink($content), NULL, Freq::WEEKLY);
+                           }
+                        }
 
 			return $map->toString();
 		});
