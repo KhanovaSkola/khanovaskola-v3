f449b807550bd36eb41af1db5e354d5e743a72e3
Daniel Hollas <danekhollas@gmail.com>
2017-10-24 04:38:04 +0200

    Update Kdyby/Google to comply with Nette 2.4.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,8 +7,12 @@ require __DIR__ . '/config/Configurator.php';
 require __DIR__ . '/shortcuts.php';
 
 $configurator = new App\Config\Configurator;
-$configurator->setDebugMode(['192.168.0.102']);
+//$configurator->setDebugMode(['192.168.0.102']);
+$configurator->setDebugMode(true);
 $configurator->enableDebugger(__DIR__ . '/../log');
+
+// Disable deprecation errors for now
+//error_reporting(~E_USER_DEPRECATED); // note ~ before E_USER_DEPRECATED
 
 $configurator->createRobotLoader()
 	->register();
