3091f5e0926d04624dbab2a6af8e20592860c2c9
Daniel Hollas <danekhollas@gmail.com>
2020-03-13 21:06:11 +0100

    [js] Remove Blueprint js and less code

--- www/js/app/blueprint-editor/default.js
+++
@@ -1,8 +0,0 @@
-define(['services/code'], function(code) {
-	require(['app/blueprint-editor/to-refactor']);
-
-	for (let $textarea of document.querySelectorAll('textarea')) {
-		code.renderCode($textarea);
-	}
-
-});
--- www/js/services/timer.js
+++
@@ -1,8 +0,0 @@
-define({
-	create: () => {
-		const then = (new Date).getTime();
-		return () => {
-			return (new Date).getTime() - then;
-		}
-	}
-});
