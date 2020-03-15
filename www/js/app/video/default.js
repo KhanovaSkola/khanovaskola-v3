eacbf3d71665a252cb8dee0b096fa823bba5ea89
Daniel Hollas <danekhollas@gmail.com>
2020-03-15 05:12:43 +0100

    [js] Remove Js video tracking via AJAX
    We were tracking user actions when video,
    but that is no longer needed as we do not support user registration.
    
    We'll remove the PhP part of things next
    (app/presenters/Js.php and app/models/structs/VideoEvents/)
    
    We should get a lot less POST requests from now on. :-)
    (one POST per 8s of video it seems, + every user action)

--- www/js/app/video/default.js
+++ www/js/app/video/default.js
@@ -2,13 +2,11 @@ define([
 	'logic/video/fullscreen',
 	'logic/video/player',
 	'logic/video/progress',
-	'logic/video/remote',
 	'logic/video/subtitles',
-	'services/timer',
 	'logic/video/clickCatcher',
 	'logic/video/controls',
 	'logic/video/report'
-], function(fullscreen, player, progress, remote, subtitles, timer) {
+], function(fullscreen, player, progress, subtitles) {
 	const $wrapper = document.querySelector('.video-wrapper');
 
 	const $videoPreview = document.querySelector('.video-preview');
@@ -17,40 +15,10 @@ define([
 	const $videoReal = document.querySelector('.video-real');
 	const $overlayPlayButton = document.querySelector('.video-wrapper .video-play');
 
-	let secondsWatched = 0;
-	let furthest = 0;
-	let watched = false;
-	let pause = {
-		at: null,
-		timer: null
-	};
 	let overlayVisible = true;
 
 	subtitles.load($wrapper.dataset.subtitlesUrl);
 	player.init();
-
-
-	const sendPosition = function(time) {
-		secondsWatched += player.delta.long / 1000;
-		if (time > furthest) {
-			furthest = time;
-		}
-
-		const percent = secondsWatched / player.video.duration * 100;
-		remote.call('tick', {
-			percent: percent === Infinity ? 0 : percent,
-			time: secondsWatched,
-			furthest: furthest,
-			watched: watched
-		}, payload => {
-			watched = payload.watched;
-		});
-	};
-
-
-	player.onInit.push(() => {
-		remote.call('init');
-	});
 
 	player.onPlay.push(() => {
 		if (overlayVisible) {
@@ -62,19 +30,6 @@ define([
 
 			overlayVisible = false;
 		}
-
-		if (pause.at) {
-			remote.call('pause', {
-				at: pause.at,
-				duration: pause.timer() / 1000
-			});
-		}
-	});
-
-	player.onPause.push(time => {
-		pause.at = time;
-		pause.timer = timer.create();
-		sendPosition(time);
 	});
 
 	player.onShortTick.push(time => {
@@ -82,23 +37,7 @@ define([
 		progress.update(time);
 	});
 
-	player.onLongTick.push(sendPosition);
-
 	player.onSeek.push((time, before) => {
 		progress.update(time);
-
-		remote.call('seek', {
-			from: before,
-			to: time
-		});
-	});
-
-	player.onStop.push(sendPosition);
-
-	fullscreen.onChange.push((time, status) => {
-		remote.call('init', {
-			at: time,
-			isFullscreenNow: status
-		});
 	});
 });
