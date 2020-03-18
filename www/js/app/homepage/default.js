11cffc4cfc2f292f3c76df111ea86575d17870da
Daniel Hollas <danekhollas@gmail.com>
2020-03-18 21:22:12 +0100

    [js] FIX: Begin button should open burger menu

--- www/js/app/homepage/default.js
+++ www/js/app/homepage/default.js
@@ -1,9 +1,11 @@
 define(['logic/header/dropdowns'], function(dropdowns) {
 	const $beginButton = document.querySelector('[data-begin]');
 	const $dropdown = document.querySelector('[data-subjects-dropdown]');
+	const $mobileNav = document.querySelector('.mobile-nav');
 
 	$beginButton.addEventListener('click', event => {
 		dropdowns.openDropdown($dropdown);
+		$mobileNav.classList.add('open');
 
 		window.scrollTo(0, 0);
 		event.preventDefault();
