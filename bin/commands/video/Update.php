43e31c597150b6c17215e177ee72011baca8ef5c
Daniel Hollas <danekhollas@gmail.com>
2020-03-29 01:29:57 +0100

    Add optional YouTubeID param to video:update command
    Excellent if we need to manually update thumbnails/duration
    for specific videos.
    Usage:
    php www/index.php video:update youtube_id

--- bin/commands/video/Update.php
+++ bin/commands/video/Update.php
@@ -7,6 +7,7 @@ use App\Models\Rme\Video;
 use App\Models\Services\Youtube;
 use Bin\Commands\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
 
 
 class Update extends Command
@@ -15,20 +16,29 @@ class Update extends Command
 	protected function configure()
 	{
 		$this->setName('video:update')
-			->setDescription('Set preview image url and duration for videos that dont have them');
+			->setDescription('Set preview image url and duration for videos that dont have them')
+			->addArgument('youtube_id', InputArgument::OPTIONAL,
+				'metadata for video with this YouTubeID will be updated');
 	}
 
 	public function invoke(RepositoryContainer $orm, Youtube $youtube)
 	{
-		$this->updatePreviews($orm, $youtube);
-		$this->updateDurations($orm, $youtube);
+		$youtube_id = $this->in->getArgument('youtube_id');
+
+		$this->updatePreviews($orm, $youtube, $youtube_id);
+		$this->updateDurations($orm, $youtube, $youtube_id);
 	}
 
-	protected function updatePreviews(RepositoryContainer $orm, Youtube $youtube)
+	protected function updatePreviews(RepositoryContainer $orm, Youtube $youtube, $youtube_id)
 	{
 		$count = 0;
 
-		$videos = $orm->contents->findAllVideos()->findBy(['preview' => NULL]);
+    if ($youtube_id) {
+		  $videos = $orm->contents->findAllVideos()->findBy(['youtube_id' => $youtube_id]);
+    } else {
+		  $videos = $orm->contents->findAllVideos()->findBy(['preview' => NULL]);
+    }
+
 		$progress = new ProgressBar($this->out, $videos->count());
 		$progress->start();
 
@@ -46,11 +56,17 @@ class Update extends Command
 		$this->out->writeln("<info>Preview image updated for $count videos.</info>");
 	}
 
-	protected function updateDurations(RepositoryContainer $orm, Youtube $youtube)
+	protected function updateDurations(RepositoryContainer $orm, Youtube $youtube, $youtube_id)
 	{
+
 		$count = 0;
 
-		$videos = $orm->contents->findVideosWithoutDuration();
+    if ($youtube_id) {
+		  $videos = $orm->contents->findAllVideos()->findBy(['youtube_id' => $youtube_id]);
+    } else {
+		  $videos = $orm->contents->findVideosWithoutDuration();
+    }
+
 		$progress = new ProgressBar($this->out, $videos->count());
 		$progress->start();
 
