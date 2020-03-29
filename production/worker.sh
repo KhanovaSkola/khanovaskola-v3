934489e9685b8996d42d6710cfe747f0c3d3c5ed
Daniel Hollas <danekhollas@gmail.com>
2020-03-29 06:54:11 +0200

    CRITICAL FIX: Fix mem leak from RabbitMQ workers
    After Kdyby/RabbitMQ update in 4edd6d0df
    the www/index.php ra:con command started ignoring INT signal
    so each minute cron job was adding new process, eventually leading
    to RAM saturation and system freeze
    
    This is fixed by the -w parameter,
    I also added memory limit just in case

--- production/worker.sh
+++ production/worker.sh
@@ -7,9 +7,8 @@
 # If no task ever hit the consumer however, it does not
 # handle the INT signal and we must kill it manually.
 
-
 cd /srv/khanovaskola.cz/production
 
-timeout --signal=INT 58 php www/index.php ra:con mail &
-timeout --signal=INT 58 php www/index.php ra:con updateSchema &
-timeout --signal=INT 58 php www/index.php ra:con updateVideo &
+timeout --signal=INT 58 php www/index.php ra:con -w -l 50 mail &
+timeout --signal=INT 58 php www/index.php ra:con -w -l 50 updateSchema &
+timeout --signal=INT 58 php www/index.php ra:con -w -l 50 updateVideo &
